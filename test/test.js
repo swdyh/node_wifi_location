4275ee5e015c05662054f37e31ef1fca0ef4014c
swdyh <youhei@gmail.com>
2012-08-11 17:59:03 +0900

    add test script

---
+++ test/test.js
@@ -0,0 +1,41 @@
+var fs = require('fs')
+var path = require('path')
+var assert = require('assert')
+var wifiLocation = require(path.join(__dirname, '..', 'lib', 'wifi_location'))
+
+function testParseAirpotOutput(t) {
+    var d = wifiLocation.parseAirportOutput(t)
+    assert.equal(d.length, 24)
+    assert.ok(d[0].SSID)
+    assert.ok(d[0].BSSID)
+    assert.ok(d[0].RSSI)
+}
+
+function testParseIwlistOutput(t) {
+    var d = wifiLocation.parseIwlistOutput(t)
+    assert.equal(d.length, 5)
+    assert.ok(d[0].Address)
+    assert.ok(d[0].ESSID)
+    assert.ok(d[0]['Signal level'])
+}
+
+function testWifiTowers(t, platform) {
+    wifiLocation.wifiTowers({ t: t, platform: platform }, function(err, val) {
+        assert(val.length > 1)
+        assert.ok(val[0].ssid)
+        assert.ok(val[0].mac_address)
+        assert.ok(val[0].signal_strength)
+    })
+}
+
+function loadOutputText(file) {
+    return fs.readFileSync(path.join(__dirname, file)).toString()
+}
+
+testParseAirpotOutput(loadOutputText('osx_airport_output.txt'))
+testParseIwlistOutput(loadOutputText('linux_iwlist_output.txt'))
+testParseIwlistOutput(loadOutputText('linux_iwlist_output_eq.txt'))
+testWifiTowers(loadOutputText('osx_airport_output.txt'), 'darwin')
+testWifiTowers(loadOutputText('linux_iwlist_output.txt'), 'linux')
+testWifiTowers(loadOutputText('linux_iwlist_output_eq.txt'), 'linux')
+
